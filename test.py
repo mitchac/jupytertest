a74167009684c684ac80f2c8813787f9d48b0547
Ubuntu <ubuntu@snaptest.novalocal>
2020-01-02 02:35:32 +0000

    Add existing file

---
+++ test.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.1
+#   kernelspec:
+#     display_name: Bash
+#     language: bash
+#     name: bash
+# ---
+
+cd ~/jupyter
+
+ls
+
+export PATH=/home/ubuntu:$PATH
+
+# then check to see that *nextflow* 
+# is installed
+
+nextflow -v
